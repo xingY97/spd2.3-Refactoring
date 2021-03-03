9bcd004052c11b037693f06309533c579c76acd8
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-03-02 20:09:38 -0500

    pytest

---
+++ pytest_tut/test_one.py
@@ -0,0 +1,10 @@
+# test_one.py
+
+def calculate_kinetic_energy(mass, velocity): 
+    """Returns kinetic energy of mass [kg] with velocity [ms]."""
+    return 0.5 * mass * velocity ** 2
+
+def test_calculate_kinetic_energy():
+    mass = 10 # [kg]
+    velocity = 4 # [m/s]
+    assert calculate_kinetic_energy(mass, velocity) == 80
